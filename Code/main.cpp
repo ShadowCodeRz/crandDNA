87295fa3c7d3dced621f3ee1bae9234d52315d7a
ShadowCodeRz <24922476+ShadowCodeRz@users.noreply.github.com>
2019-02-16 15:17:05 -0400

    Update main.cpp

--- Code/main.cpp
+++ Code/main.cpp
@@ -16,7 +16,6 @@ required, welp. Anywho my implementaion only uses the 4 characters that he
 said^^^^^ sooooo the input seed for the bases is useless and i removed it
 */
 
-
 string randDNA(int seed, int n) {
 	mt19937 eng1(seed);
 	int min = 0; // minimum number to generate
@@ -31,19 +30,47 @@ string randDNA(int seed, int n) {
 		index = uniform(eng1);
 		dna = dna + DnaBases[index];
 	}// adds A C G T to the output string
+	return dna;// returns the output string
+}
 
+string randDNA(int seed, string bases, int n) {
+	mt19937 eng1(seed);
+	int min = 0; // minimum number to generate
+	int max = bases.size() - 1; // maximum number to generate which would be the size of the string
+	uniform_int_distribution<> uniform(min, max);// random stuff to get random number generation to work
+	int index = 0;
+	string dna = "";
 
+	for (int i = 0; i < n; i++)
+	{
+		index = uniform(eng1);
+		dna = dna + bases[index];
+	}// adds A C G T to the output string
 	return dna;// returns the output string
 }
 
 int main(){
 	int seed = -1, n = -1;
+	string Input = "";
+	string bases = "";
+	cout << "Do you want to use the normal dna letters(N) or enter your own(O)?" << endl;
+	cin >> Input;
 
-
-	cout << "\nWelcome to the DNA generator\n\n\tPlease enter a seed\n\n\t>>> ";
-	cin >> seed;
-	cout << "\n\tand the length of the DNA sequence to be generated\n\n\t>>> ";
-	cin >> n;
-	cout << "\n\nOutput:\n\t" << randDNA(seed, n) << endl; //print output  
+	if (Input == "N") {
+		cout << "\nWelcome to the DNA generator\n\n\tPlease enter a seed\n\n\t>>> ";
+		cin >> seed;
+		cout << "\n\tand the length of the DNA sequence to be generated\n\n\t>>> ";
+		cin >> n;
+		cout << "\n\nOutput:\n\t" << randDNA(seed, n) << endl; //print output  
+	}
+	else {
+		cout << "\nWelcome to the DNA generator\n\n\tPlease enter a seed\n\n\t>>> ";
+		cin >> seed;
+		cout << "\n\tand the letters of the DNA sequence to be generated\n\n\t>>> ";
+		cin >> bases;
+		cout << "\n\tand the length of the DNA sequence to be generated\n\n\t>>> ";
+		cin >> n;
+		cout << "\n\nOutput:\n\t" << randDNA(seed, bases, n) << endl; //print output  
+	}
 	system("pause");
 }
